82d42cbf7b496243056c0f3f4f5625c98d0c9d9a
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-15 15:07:53 +0100

    sanitize contenteditable content for textline

--- plonetheme/foundation/browser/foundation.js
+++ plonetheme/foundation/browser/foundation.js
@@ -1,1 +1,11 @@
 //some javascript specific for the theme.
+(function($) {
+    $().ready(function() {
+    	var toremoves = ('<div>', '</div>', '<br>', '<br/>');
+    	$('div[contenteditable].textline-field').bind('input', function(event){
+    		for (toremove in toremoves){
+        		event.originalEvent.srcElement.innerHTML = event.originalEvent.srcElement.innerHTML.replace(toremove, '');
+    		}
+    	});
+    });
+})(jQuery);
