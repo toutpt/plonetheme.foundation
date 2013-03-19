0ebf96592d357aee0dec4d2c28bd1bb8ad0309f7
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-19 16:05:28 +0100

    fix the main grid by placing content first

--- plonetheme/foundation/browser/foundation_view.py
+++ plonetheme/foundation/browser/foundation_view.py
@@ -44,31 +44,31 @@ class FoundationView(BrowserView):
 
         # pre-fill dictionary
         columns = dict(one="", content="", two="")
-        portlets = "large-3 columns"
+        portlets = "small-12 large-3 columns"
         if not sl and not sr:
             # we don't have columns, thus conten takes the whole width
-            columns['content'] = "large-12 columns"
+            columns['content'] = "small-12 columns"
 
         elif sl and sr:
             # In case we have both columns, content takes 50% of the whole
             # width and the rest 50% is spread between the columns
-            columns['one'] = portlets
-            columns['content'] = "large-6 columns"
+            columns['one'] = portlets + " pull-6"
+            columns['content'] = "small-12 large-6 push-3 columns"
             columns['two'] = portlets
 
         elif (sr and not sl) and not isRTL:
             # We have right column and we are NOT in RTL language
-            columns['content'] = "large-9 columns"
+            columns['content'] = "large-9 small-12 columns"
             columns['two'] = portlets
 
         elif (sl and not sr) and isRTL:
             # We have left column and we are in RTL language
-            columns['one'] = portlets
-            columns['content'] = "large-9 columns"
+            columns['one'] = portlets + " pull-6"
+            columns['content'] = "large-9 small-12 push-3 columns"
 
         elif (sl and not sr) and not isRTL:
             # We have left column and we are in NOT RTL language
-            columns['one'] = portlets
-            columns['content'] = "large-9 columns"
+            columns['one'] = portlets + " pull-6"
+            columns['content'] = "large-9 small-12 push-3 columns"
 
         return columns
