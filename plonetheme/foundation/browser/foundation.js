2222e62095cfaed2b1889c63ca0f15b96f78a8da
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-19 12:03:18 +0100

    use foundation fixes from github issue tracker. remove uneeded tags from the search

--- plonetheme/foundation/browser/foundation.js
+++ plonetheme/foundation/browser/foundation.js
@@ -7,5 +7,12 @@
         		event.originalEvent.srcElement.innerHTML = event.originalEvent.srcElement.innerHTML.replace(toremove, '');
     		}
     	});
+    	if ($('[autofocus]').length == 0){
+    		if ($('#form-widgets-IDublinCore-title').length != 0){
+    			$('#form-widgets-IDublinCore-title').prop('autofocus', true);
+    		}else{
+        		$('#searchGadget').prop('autofocus', true);
+    		}
+    	}
     });
 })(jQuery);
