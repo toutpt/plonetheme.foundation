89d233c65631de12aff9d8c92b8e51feeee527b1
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-27 16:49:37 +0100

    remove presentation and override byline

--- plonetheme/foundation/browser/dashboard.py
+++ plonetheme/foundation/browser/dashboard.py
@@ -2,6 +2,7 @@ from plone.app.layout.dashboard.dashboard import DashboardView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
 from Products.CMFPlone import PloneMessageFactory as _p
+from zope.i18n import translate
 
 
 class FoundationDashboard(DashboardView):
@@ -18,5 +19,6 @@ class FoundationDashboard(DashboardView):
         self.request.set('disable_plone.rightcolumn', 1)
         if self.empty() and self.can_edit():
             status = IStatusMessage(self.request)
-            msg = self.context.translate(_p(u"info_empty_dashboard"))
+            msg = _p(u"info_empty_dashboard")
+            msg = translate(msg, domain='plone', context=self.request)
             status.add(msg, "info")
