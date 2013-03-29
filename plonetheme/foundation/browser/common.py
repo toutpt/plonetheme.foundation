3f2ed68fbefff6e35a5cc8fe7cc5bc8c03e47f77
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-29 10:17:05 +0100

    remove selected view from the dropdown menu

--- plonetheme/foundation/browser/common.py
+++ plonetheme/foundation/browser/common.py
@@ -1,7 +1,42 @@
+from zope import component
 from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
 
 
-class DisableBorderView(BrowserView):
+class BaseView(BrowserView):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.cached_tools = {}
+        self.cached_components = {}
+
+    def __call__(self):
+        self.update()
+        return self.index()
+
+    def update(self):
+        pass
+
+    def get_tool(self, toolname):
+        if not toolname in self.cached_tools:
+            self.cached_tools[toolname] = getToolByName(self.context, toolname)
+        return self.cached_tools[toolname]
+
+    def get_component(self, name, context=None):
+        if type(context) == tuple:
+            getcomponent = component.queryMultiAdapter
+        else:
+            getcomponent = component.queryAdapter
+        if context is None:
+            context = self.context
+        self.cached_components[name] = getcomponent(context, name=name)
+        return self.cached_components[name]
+
+    def get_view(self, name):
+        return self.get_component(name, (self.context, self.request))
+
+
+class DisableBorderView(BaseView):
     """Disable border of the view"""
 
     def update(self):
