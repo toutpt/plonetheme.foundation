3be27b38fe5a7c0295f54168cc954aed7849d5c3
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-03 18:18:04 +0200

    refactor: change the way to remove files
    Include default bundle in the theme but override all resources.
    We start the process to include plone JS / CSS one by one.

--- plonetheme/foundation/upgrades.py
+++ plonetheme/foundation/upgrades.py
@@ -3,5 +3,11 @@ PROFILEID = 'profile-plonetheme.foundation:default'
 
 
 def common(context):
+    cleanup(context)
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILEID)
+
+
+def cleanup(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile("profile-plonetheme.foundation:zclean")
